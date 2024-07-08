1efaeb1f88703c16cdfcffb1de804ee9de7919ee
Soham Dasgupta <soham@thesobercoder.in>
2024-07-08 23:13:09 +0530

    feat: calculate bash path dynamically from environment variables

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,11 +5,7 @@
 -- Set shell to bash on Windows
 local is_windows = vim.loop.os_uname().sysname == "Windows"
 if is_windows then
-  local scoop_shims = os.getenv("USERPROFILE") .. "\\.scoop\\shims\\bash.exe"
-  local file = io.open(scoop_shims, "r")
-  if file then
-    file:close()
-    vim.opt.shell = scoop_shims
-    vim.opt.shellcmdflag = "-c"
-  end
+  local bash_path = os.getenv("USERPROFILE") .. "\\.scoop\\shims\\bash.exe"
+  vim.opt.shell = bash_path
+  vim.opt.shellcmdflag = "-c"
 end
