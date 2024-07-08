878dc9c5eb9415f1b462e1fca4997b4ec2b16db2
Soham Dasgupta <soham@thesobercoder.in>
2024-07-08 22:40:28 +0530

    feat: add option to use bash on Windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set shell to bash on Windows
+local is_windows = vim.loop.os_uname().sysname == "Windows"
+if is_windows then
+  vim.opts.shell = "<Path to bash>/bash.exe"
+  vim.opts.shellcmdflag = "-c"
+end
