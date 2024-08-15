09347fd9f678105509cc2f748a20a151701e2216
Soham Dasgupta <soham@thesobercoder.in>
2024-08-16 01:21:27 +0530

    feat: customize code style

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,17 @@
 return {
-  { "navarasu/onedark.nvim" },
+  {
+    "navarasu/onedark.nvim",
+    config = function()
+      require("onedark").setup({
+        code_style = {
+          comments = "italic",
+          keywords = "italic",
+          functions = "italic",
+        },
+      })
+      require("onedark").load()
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
